54f082385810ef30a4b057753528a4f06179077b
aniruddh-pc <aniruddh1216@gmail.com>
2025-10-15 14:07:21 +0530

    Updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
  import axios from "axios";
                     
  const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todolist-3-gylt.onrender.com/api",
  });
 
  api.interceptors.request.use(config=>{
